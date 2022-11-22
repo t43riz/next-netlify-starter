7c39658d76ef0385c56a4e77c7ee2c8b97cbe254
t43riz <t43riz@users.noreply.github.com>
2022-11-22 14:03:37 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -147,36 +147,72 @@ export default function Home() {
         }
     }())
 </script>
-  <section class="blk-sect">
-    <div class="container-width">
-      <div class="blk-title">Blocks
+  <div id="icax">
+    <div class="container mx-auto px-8 py-4 my-3">
+      <h1 class="p-2 text-5xl leading-relaxed font-bold text-center">New 2022 Senior Relief Hacks
+        <br/>
+      </h1>
+      <div class="full-width">
+        <a href="https://path.searchformoney.io/lander_pages/29" id="ivyi"></a>
       </div>
-      <div class="blk-desc">Each element in HTML page could be seen as a block. On the left panel you could find different kind of blocks that you can create, move and style
+      <hr size="5" color="#cccccc" id="ihrjg"/>
+      <h3 class="p-2 text-3xl leading-relaxed font-bold text-center">
+        <a href="https://www.andd56trk.com/cmp/5657H/6G8W241/?source_id=1090095" id="ip195">Seniors Over 64 are Taking Advantage of This $1,200 Giveback Program<br/></a>
+      </h3>
+      <div data-gjs="redirectable_element" data-controller="link-redirect" data-link-redirect-current-tab-url-value="https://www.andd56trk.com/cmp/5657H/6G8W241/?source_id=1090095" data-link-redirect-new-tab-url-value="" data-action="click->link-redirect#interceptClickWithRedirect" class="cursor-pointer">
+        <img id="iputl" src="https://landeradmin.s3.amazonaws.com/uploads/image/url/21/imgonline-com-ua-compressed-6mugBTGKEO.jpg"/>
       </div>
-      <div class="price-cards">
-        <div class="price-card-cont">
-          <div class="price-card pc-enterprise">
-            <div class="pc-title">Enterprise
-            </div>
-            <div class="pc-desc" id="i41xs">Some random list
-            </div>
-            <div class="pc-feature odd-feat">+ Enterprise feature 1
-            </div>
-            <div class="pc-feature">+ Enterprise feature 2
-            </div>
-            <div class="pc-feature odd-feat">+ Enterprise feature 3
-            </div>
-            <div class="pc-feature" id="iv1uo">+ Enterprise feature 4
-            </div>
-          </div>
+      <p class="py-2 text-lg">If you&#039;re a senior that&#039;s on medicare, you could be due an extra $1,200 from this medicare supplement B giveback program. This can help you cover grocery and OTC costs. Dental coverage can also even be included in the benefits you receive. Just click the link below to check out what you could qualify for!
+        <br/>
+      </p>
+      <div data-gjs="redirectable_element" data-controller="link-redirect" data-link-redirect-current-tab-url-value="https://www.andd56trk.com/cmp/5657H/6G8W241/?source_id=1090095" data-action="click->link-redirect#interceptClickWithRedirect" class="cursor-pointer">
+        <button data-controller="button-base" data-action="button-base#click" class="shadow rounded w-full py-4 my-2 text-2lg text-white bg-primary text-[#fff]">Claim Now</button>
+      </div>
+      <hr size="5" color="#cccccc" id="i7rqi"/>
+      <h3 class="p-2 text-3xl leading-relaxed font-bold text-center">
+        <a href="https://www.andd56trk.com/cmp/5657H/6GB4X4G/?source_id=1090095" id="ijfr6">Safe Drivers Paying More Than $29/Mo for Car Insurance are Putting More than $700 Back in Their Pockets with This Hack</a>
+        <br/>
+      </h3>
+      <div data-gjs="redirectable_element" data-controller="link-redirect" data-link-redirect-current-tab-url-value="https://www.andd56trk.com/cmp/5657H/6GB4X4G/?source_id=1090095" data-link-redirect-new-tab-url-value="" data-action="click->link-redirect#interceptClickWithRedirect" class="cursor-pointer">
+        <img src="https://landeradmin.s3.amazonaws.com/uploads/image/url/22/Usdpfwrpug__1_f.jpeg" id="iufj9"/>
+      </div>
+      <div data-gjs="redirectable_element" data-controller="link-redirect" data-link-redirect-current-tab-url-value="https://www.andd56trk.com/cmp/5657H/6CJTJ3N/?source_id=690045" data-link-redirect-new-tab-url-value="" data-action="click->link-redirect#interceptClickWithRedirect" class="cursor-pointer">
+      </div>
+      <p class="py-2 text-lg">It is widely known that many people pay far too much for their car insurance. Generally, depending on age and driving history, individuals could qualify for huge discounts &amp; savings on their policies. The problem is that most fail to compare and simply let their existing policies renew year after year... For most, if you pay more than $29/month for car insurance, you are very likely overpaying for your policy.
+        <br/>
+        <br/>Thanks to this innovative new website, most Americans can take advantage of up to $712 in savings which may be available, yet most don’t even know about! The site is super easy to use and allows you to see instantly how much lower your new rate is. Click here to see if you qualify to for up to $712 back in your pockets.
+      </p>
+      <div data-gjs="redirectable_element" data-controller="link-redirect" data-link-redirect-current-tab-url-value="https://www.andd56trk.com/cmp/5657H/6GB4X4G/?source_id=1090095" data-action="click->link-redirect#interceptClickWithRedirect" class="cursor-pointer">
+        <button data-controller="button-base" data-action="button-base#click" class="shadow rounded w-full py-4 my-2 text-2lg text-white bg-primary text-[#fff]">Claim Now</button>
+      </div>
+      <hr size="5" color="#cccccc" id="imzrv"/>
+      <div data-gjs="redirectable_element" data-controller="link-redirect" data-link-redirect-current-tab-url-value="https://www.andd56trk.com/cmp/5657H/6GCCS4X/?source_id=1090095" data-link-redirect-new-tab-url-value="" data-action="click->link-redirect#interceptClickWithRedirect" class="cursor-pointer">
+        <div data-gjs="redirectable_element" data-controller="link-redirect" data-link-redirect-current-tab-url-value="https://www.andd56trk.com/cmp/5657H/6GCCS4X/?source_id=1090095" data-action="click->link-redirect#interceptClickWithRedirect" class="cursor-pointer">
         </div>
+        <hr size="5" color="#cccccc" id="iz09b"/>
       </div>
     </div>
-  </section>
-  <section class="bdg-sect">
-    <div class="container-width">
+  </div>
+  <footer class="progressbar-template-footer flex justify-start items-center flex-col h-auto bg-gray-100 w-full font-opensans">
+    <div class="legal-wrapper w-4/5 text-sm font-light leading-normal mx-auto md:max-w-screen-lg md:px-12">
+      <p class="text-center text-xxs leading-tight mb-4 lg:mb-2 lg:text-xs">
+        <br/>The information above has been fact-checked and updated for accuracy.
+        <br/>
+        <br/>This page is an advertorial, and not an actual news article, blog or consumer protection update. This is not a government program nor is it government aid, this is an advertorial for a loan service. T&amp;C Apply, see site for details!
+        <br/>
+        <br/>Trademarks utilized on our website belong to their respective owners and no implied or expressed endorsement of our website or services is intended. Through in-depth research and experienced editors we provide feedback about products and services. We are independently owned, and opinions expressed here are our own.
+        <br/>
+        <br/>THIS IS AN ADVERTORIAL AND NOT AN ACTUAL NEWS ARTICLE, BLOG, OR CONSUMER PROTECTION UPDATE. We are dedicated to bringing readers valuable information which can help them accomplish their financial and lifestyle goals. Our disclaimer is that this site does receive compensation for product reviews and referrals or purchases made through our links. This page is an advertisement/advertorial. The story depicted here is for demonstration purposes only and everyone&#039;s results may vary. We hope you find our online resource informative and helpful. This site is in no way affiliated with any news source. This site contains affiliate and partner links. This website and the company that owns it is not responsible for any typographical or photographic errors. If you do not agree to our terms and policies, then please leave this site immediately. All trademarks, logos, and service marks (collectively the &quot;Trademarks&quot;) displayed are registered and/or unregistered Trademarks of their respective owners. Contents of this website are copyrighted property of the reviewer and/or this website. 
+      </p>
+      <p class="text-center text-xxs mb-4 lg:mb-2 lg:text-xs">
+        <br/>© 2022 path.yourbenefitsworth
+      </p>
     </div>
-  </section>
+    <div class="menu mb-4">
+      <a href="https://yourbenefitsworth.com/terms" class="text-center text-xs text-gray-400 mx-1">Terms and Service</a>
+      <a href="https://yourbenefitsworth.com/privacy" class="text-center text-xs text-gray-400 mx-1">Privacy Policy</a>
+    </div>
+  </footer>
 </body>
   )
 }
