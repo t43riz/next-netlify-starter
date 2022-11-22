20c0454fd47344d2181f9c7a16b436b8b9abd03c
t43riz <t43riz@users.noreply.github.com>
2022-11-22 14:53:39 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,12 +4,13 @@ export default function Document() {
   return (
     <Html>
 <Head>
-  <script type="text/javascript" dangerouslySetInnerHTML={{ __html: process.env.rawJsFromFile }}></script>
+
 </Head>
       <body>
         <Main />
         <NextScript />
       </body>
+      <script type="text/javascript" dangerouslySetInnerHTML={{ __html: process.env.rawJsFromFile }}></script>
     </Html>
   )
 }
